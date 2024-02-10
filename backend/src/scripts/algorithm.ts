7a763a0feda222383ffa35adbf91e4cf5bfb43f2
user2oo4 <hiepphamm04@gmail.com>
2024-02-10 10:44:29 -0500

    changed algorithm and package-lock

--- backend/src/scripts/algorithm.ts
+++ backend/src/scripts/algorithm.ts
@@ -1,128 +1,123 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
-// Define a function to get the rating of a professor
-function getProfessorRating(professor) {
-  // Implementation for getting the professor's rating
-  return 0
-}
-
-
-// Define a function to process the time frame of a class
-function processTime(classX) {
-  // Implementation for processing the time frame
+class Professor {
+    public firstName: String
+    public lastName: String
+    public rating: number
+    constructor(firstName: String, lastName: String, rating: number) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.rating = rating
+    }
 }
 
 // Define the class structure
 class Class {
-  constructor(courseNumber, professor, crn, startTime, endTime, location) {
-    this.courseNumber = courseNumber;
-    this.professor = professor;
-    this.crn = crn;
-    this.startTime = startTime; // vector<int>[5]: M, T, W, R, F
-    this.endTime = endTime; // vector<int>[5]: M, T, W, R, F
-    this.location = location; // string
-  }
+    public courseNumber: String
+    public courseName: String
+    public professor: Professor
+    public crn: number
+    public startTime: number[]
+    public endTime: number[]
+    public location: String
+    constructor(courseNumber: String, courseName: String, professor: Professor, 
+        crn: number, timeFrame: String, day: String, location: String) {
+        this.courseNumber = courseNumber;
+        this.courseName = courseName;
+        this.professor = professor;
+        this.crn = crn;
+        this.startTime = [0, 0, 0, 0, 0]
+        this.endTime = [0, 0, 0, 0, 0]
+        for (let i = 0 ; i < day.length ; i++) {
+            let id = 0; // M
+            if(day == 'M') id = 0;
+            else if(day == 'T') id = 1;
+            else if(day == 'W') id = 2;
+            else if(day == 'R') id = 3;
+            else if(day == 'F') id = 4;
+            this.startTime[id] = parseInt(timeFrame.substring(0, 2)) * 60 + parseInt(timeFrame.substring(3, 4)); // vector<int>[5]: M, T, W, R, F
+            this.endTime[id] = parseInt(timeFrame.substring(5, 7)) * 60 + parseInt(timeFrame.substring(7, 9)); // vector<int>[5]: M, T, W, R, F
+        }
+        this.location = location; // string
+    }
 }
-
-// Define the Schedule structure
+  
+  // Define the Schedule structure
 class Schedule {
-  constructor(averageProfessorRating, minProfessorRating, classes) {
-    this.averageProfessorRating = averageProfessorRating;
-    this.minProfessorRating = minProfessorRating;
-    this.classes = classes; // Vector of Class instances
-  }
-}
-
-// Vector of vectors to store groups of classes
-let vt = [];
-
-// Function to sort the vector of classes
-function sortClasses() {
-  // Implementation for sorting classes
+    public classes: Class[]
+    public numClasses: number
+    public totalTimeOnCampus: number
+    public avgProfessorRating: number
+    public totalDistance: number
+    constructor(classes: Class[]) {
+        this.classes = classes
+        this.numClasses = classes.length
+        // write later 
+        this.totalTimeOnCampus = 0
+        let sumProfRating = 0
+        for(let i = 0 ; i < this.numClasses ; i++) {
+            sumProfRating += classes[i].professor.rating
+        }
+        this.avgProfessorRating = sumProfRating / this.numClasses
+        // write later
+        this.totalDistance = 0
+    }
 }
 
 // Function to check overlapping
-function overlapping(a1, b1, a2, b2) {
-  if (a1 > a2) {
-    [a1, a2] = [a2, a1]
-  }
-  // a1 <= a2
-  // a1 <= b1
-  if (b1 >= a2) return true
-  else return false
+function overlapping(a1: number, b1: number, a2: number, b2: number) {
+    if (a1 > a2) {
+        [a1, a2] = [a2, a1];
+    }
+    // a1 <= a2
+    // a1 <= b1
+    if (b1 >= a2) return true;
+    else return false;
 }
 
 // Function to check if there's a conflict in time with the current class
-function notConflictTime(classX, classY) {
-  // Implementation for checking time conflicts
-  let startTimeX = classX.startTime
-  let endTimeX = classX.endTime
-  let startTimeY = classY.startTime
-  let endTimeY = classY.endTime
-  for (let i = 0 ; i < 5 ; i++) {
-    let st1 = startTimeX[i]
-    if (st1 == 0) continue
-    let en1 = endTimeX[i]
-    let st2 = startTimeY[i]
-    if (st2 == 0) continue
-    let en2 = endTimeY[i]
-    if (overlapping(st1, en1, st2, en2)) return false
-  }
-  return true
-}
-
-let curSchedule = []
-
-// Function to take a class
-function take(classX) {
-  // Implementation for taking a class
-  curSchedule.push(classX)
-}
-
-// Function to untake a class
-function untake(classX) {
-  // Implementation for untaking a class
-}
-
-// Check if ClassX is ok to take
-function Ok(classX) {
-  
-}
-
-
-// Function to make the schedule recursively
-function makeSchedule(pos) {
-  if (pos === vt.length) {
-    // Get the schedule here
-  }
-  for (let c of vt[pos]) {
-    if (Ok(c)) {
-      curSchedule
-      makeSchedule(pos + 1);
-      untake(c);
+function ConflictTime(classX: Class, classY: Class) {
+    for(let i = 0 ; i < 5 ; i++) {
+        if(classX.startTime[i] == 0 || classY.startTime[i] == 0) continue;
+        if(overlapping(classX.startTime[i], classX.endTime[i], classY.startTime[i], classY.endTime[i])) return true;
     }
-  }
+    return false;
 }
+
+let curSchedule: Class[] = [];
+let ClassGroups: Class[][];
+let schedules: Schedule[]
+
+// Check if class can be pushed in
+function pushable(classX: Class) {
+    for(let i = 0 ; i < curSchedule.length ; i++) {
+        if(ConflictTime(classX, curSchedule[i])) return false;
+    }
+    return true;
+}
+
+// Function to make schedule
+function makeSchedule(i: number) {
+    if(i == ClassGroups.length) {
+        schedules.push(new Schedule(curSchedule));
+    }
+    let numClasses = ClassGroups[i].length
+    for (let j = 0 ; j < numClasses ; j++) {
+        let classX = ClassGroups[i][j];
+        if(pushable(classX)) {
+            curSchedule.push(classX)
+            makeSchedule(i + 1)
+            curSchedule.pop()
+        }
+    }
+}
+
+const fs = require('fs');
+
+fs.readFile('src/Course Schedule - Spring 2024_Page 1_Table.csv', 'utf8', (err: any, data: any) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  // Process the CSV data
+  console.log(data);
+});
